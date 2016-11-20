feb0116a1fac132a923cca4e31430149fb81d47a
Vejryn Shaviero <envyero@gmail.com>
2016-11-20 23:35:35 +0700

    Limit Jumlah dan Page 3

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl509182736/soalles/BuatSoalActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl509182736/soalles/BuatSoalActivity.java
@@ -1,33 +1,46 @@
 package id.sch.smktelkom_mlg.project.xirpl509182736.soalles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class BuatSoalActivity extends AppCompatActivity {
 
     private LinearLayout lnrDynamicEditTextHolder;
     private EditText etJudul, etJumlah;
-    private Button bBuat;
+    private Button bBuat,bNext;
     private TextView tvJudul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_buat_soal);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Buat Soal");
         lnrDynamicEditTextHolder = (LinearLayout) findViewById(R.id.lnrDynamicEditTextHolder);
         etJudul = (EditText) findViewById(R.id.editTextJudul);
         etJumlah = (EditText) findViewById(R.id.editTextJumlah);
+        etJumlah.setFilters(new InputFilter[]{ new InputFilterMinMax("1", "15")});
         bBuat = (Button) findViewById(R.id.buttonBuat);
+        bNext = (Button) findViewById(R.id.buttonNext);
         tvJudul = (TextView) findViewById(R.id.textViewJudul);
 
+        bNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BuatSoalActivity.this, Tampil.class));
+            }
+        });
 
         bBuat.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl509182736/soalles/Tampil.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl509182736.soalles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tampil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil);
+    }
+}
